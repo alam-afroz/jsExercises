fb8d1f498fe84544dbe03c67bc43442364944521
Afroz Alam <236445412+afrozalam0699-lab@users.noreply.github.com>
2025-12-22 16:18:00 +0530

    exercise 06 completed of jsExercises from foundations

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  let result = "";
+  if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+      result = result + word;
+    }
+    return result;
+  } else {
+    return "ERROR";
+  }
 };
+const newString = repeatString("bye", 0);
+console.log(newString);
 
 // Do not edit below this line
 module.exports = repeatString;
