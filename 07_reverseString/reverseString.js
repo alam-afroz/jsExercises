46a93962a082bf8b86f9b0a3b7787e2a9c302441
Afroz Alam <236445412+afrozalam0699-lab@users.noreply.github.com>
2025-12-23 12:08:50 +0530

    exercise 08 done for jsexercise in foundation

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  arr = word.split("");
+  let arr2 = arr.reverse();
 
+  arr3 = arr2.join("");
+  return arr3;
 };
-
+console.log(reverseString("memes"));
 // Do not edit below this line
 module.exports = reverseString;
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr, ...element) {
+  const newArray = [];
 
+  arr.forEach(function items(items) {
+    if (!element.includes(items)) {
+      newArray.push(items);
+    }
+  });
+
+  return newArray;
+};
+console.log(removeFromArray(["a", "s", "w", 3, 6, "9"], 6, "s", "9"));
 // Do not edit below this line
 module.exports = removeFromArray;
