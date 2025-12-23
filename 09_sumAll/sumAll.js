93f261d5b61052e441905e22fd0540a7bef448f8
Afroz Alam <236445412+afrozalam0699-lab@users.noreply.github.com>
2025-12-23 12:45:43 +0530

    exercise 09 done without watching solution of jsExercise of foundations

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let newNum = 0;
+  let a = num1 % 1;
+  let b = num1 % 1;
+  if (
+    typeof num1 != "number" ||
+    typeof num2 != "number" ||
+    num1 + num2 < 0 ||
+    a + b != 0
+  ) {
+    newNum = "ERROR";
+  } else if (num1 < num2) {
+    for (let i = num1; i <= num2; i++) {
+      newNum = newNum + i;
+    }
+  } else if (num2 < num1) {
+    for (let i = num2; i <= num1; i++) {
+      newNum = newNum + i;
+    }
+  } else {
+    newNum = "ERROR";
+  }
+  return newNum;
 };
-
+const sum = sumAll(1, 3);
+console.log(sum);
+console.log(sumAll(3, 1));
 // Do not edit below this line
 module.exports = sumAll;
